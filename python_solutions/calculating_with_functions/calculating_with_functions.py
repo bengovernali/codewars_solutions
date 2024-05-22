733eec0b6a5124f3ac1501f6bb217a8934e673dd
Ben Governali <bgovern93@gmail.com>
2024-05-22 11:48:51 -0700

    added calculating_with_functions

---
+++ python_solutions/calculating_with_functions/calculating_with_functions.py
@@ -0,0 +1,15 @@
+def zero(op="+ 0"): return int(eval(f"0 {op}"))
+def one(op="+ 0"): return int(eval(f"1 {op}"))
+def two(op="+ 0"): return int(eval(f"2 {op}"))
+def three(op="+ 0"): return int(eval(f"3 {op}"))
+def four(op="+ 0"): return int(eval(f"4 {op}"))
+def five(op="+ 0"): return int(eval(f"5 {op}"))
+def six(op="+ 0"): return int(eval(f"6 {op}"))
+def seven(op="+ 0"): return int(eval(f"7 {op}"))
+def eight(op="+ 0"): return int(eval(f"8 {op}"))
+def nine(op="+ 0"): return int(eval(f"9 {op}"))
+
+def plus(x): return f"+ {x}"
+def minus(x): return f"- {x}"
+def times(x): return f"* {x}"
+def divided_by(x): return f"/ {x}"
